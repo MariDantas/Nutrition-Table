a914d836e28cc9639de12a13384e401ed0952548
MariDantas <61691909+MariDantas@users.noreply.github.com>
2023-08-01 00:31:20 -0300

    Add files via upload

--- nutrition.js
+++ nutrition.js
@@ -1,12 +1,16 @@
 /**
- * @param {string} label
- * @param {string} value
+ * @param {array[][]} rows
  */
-export function renderTableRow(label, value) {
-    console.log(label, value);
-    return `<tr>
-    <td>${label}</td>
-    <td>${value}</td>
-    </tr>`;
+export function renderTableRows(rows) {
+    console.log(rows);
+    let tableRows = ``;
 
+    rows.forEach(function(row){
+        tableRows += `<tr>
+        <td>${row[0]}</td>
+        <td>${row[1]}</td>
+        </tr>`;
+    })
+
+    return tableRows;
 }
